7bd69eee87a38ad04bdaca4f5107439f676c37cb
bbb169 <827088092@qq.com>
2024-05-25 20:44:56 +0800

    feat: try to fix upload max size

--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { FileService } from './file.service';
 import { FileController } from './file.controller';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 @Module({
+  imports: [
+    MulterModule.register({
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, cb) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          cb(null, file.fieldname + '-' + uniqueSuffix);
+        },
+      }),
+      limits: { fileSize: 50 * 1024 * 1024 }, // 限制文件大小为 50MB
+    }),
+  ],
   controllers: [FileController],
   providers: [FileService],
 })
